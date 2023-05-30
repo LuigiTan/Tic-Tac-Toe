28c45945d84c86aac38ef74fae148a39766ece5c
LuigiTan <gibran.garcia@uniat.edu.mx>
2023-05-30 07:26:37 -0600

    Tic Tac toe Poco optimizado pero funcional

--- Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cstdlib>
-#include <windows.h>
+
 using namespace std;
 
 int main()
@@ -14,7 +14,12 @@ int main()
 	int Player1 = 0;
 	int Player2 = 0;
 	bool Continue = false;
-	bool Turns = false;
+	bool CheckWinner = false;
+	int TurnCount = 1;
+	//char P1 = 'X';
+	//char P2 = 'Y';
+	void WhoWon(int TurnCheck);
+	bool VictoryCheck(char BoardCheck[3][3], int LastPInput);
 	char board[ROWS][COLUMNS] = { {' ', ' ', ' '},
 								  {' ', ' ', ' '},
 								  {' ', ' ', ' '} };
@@ -22,6 +27,9 @@ int main()
 	char boardoptions[ROWS][COLUMNS] = { {'0', '1', '2'},
 										 {'3', '4', '5'},
 										 {'6', '7', '8'} };
+
+
+
 
 	cout << "Welcome to: " << endl;
 	cout << "---TIC TAC TOE--- \n";
@@ -44,278 +52,361 @@ int main()
 		}
 		cout << endl;
 	}
+
 	do
 	{
-	do
-	{
-	cout << "Player 1, select a space where to place your 'X' " << endl;
-	Continue = false;
-	cin >> Player1;
-	
-	
-		switch (Player1)
+		do
 		{
-		case 0:
-			if (board[0][0] != ' ')
+			cout << ">>>>>>>>>>>>>>>>>>>>You are on turn [ " << TurnCount << " ]<<<<<<<<<<<<<<<<<<<<" << endl;
+
+			cout << "Player 1, select a space where to place your 'X' " << endl;
+			Continue = false;
+			cin >> Player1;
+
+
+			switch (Player1)
 			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
+			case 0:
+				if (board[0][0] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[0][0] = 'x';
+					Continue = true;
+				}
+				break;
+			case 1:
+				if (board[0][1] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[0][1] = 'x';
+					Continue = true;
+				}
+				break;
+			case 2:
+				if (board[0][2] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[0][2] = 'x';
+					Continue = true;
+				}
+				break;
+			case 3:
+				if (board[1][0] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[1][0] = 'x';
+					Continue = true;
+				}
+				break;
+			case 4:
+				if (board[1][1] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[1][1] = 'x';
+					Continue = true;
+				}
+				break;
+			case 5:
+				if (board[1][2] != ' ')
+				{
+					cout << "Ese espacio ya se selecciono, elige otro" << endl;
+				}
+				else
+				{
+					board[1][2] = 'x';
+					Continue = true;
+				}
+				break;
+			case 6:
+				if (board[2][0] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[2][0] = 'x';
+					Continue = true;
+				}
+				break;
+			case 7:
+				if (board[2][1] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[2][1] = 'x';
+					Continue = true;
+				}
+				break;
+			case 8:
+				if (board[2][2] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[2][2] = 'x';
+					Continue = true;
+				}
+				break;
+			default:
+				cout << "Invalid Option" << endl;
+				break;
 			}
-			else
-			{
-				board[0][0] = 'x';
-				Continue = true;
-			}
-			break;
-		case 1:
-			if (board[0][1] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[0][1] = 'x';
-				Continue = true;
-			}
-			break;
-		case 2:
-			if (board[0][2] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[0][2] = 'x';
-				Continue = true;
-			}
-			break;
-		case 3:
-			if (board[1][0] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[1][0] = 'x';
-				Continue = true;
-			}
-			break;
-		case 4:
-			if (board[1][1] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[1][1] = 'x';
-				Continue = true;
-			}
-			break;
-		case 5:
-			if (board[1][2] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[1][2] = 'x';
-				Continue = true;
-			}
-			break;
-		case 6:
-			if (board[2][0] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[2][0] = 'x';
-				Continue = true;
-			}
-			break;
-		case 7:
-			if (board[2][1] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[2][1] = 'x';
-				Continue = true;
-			}
-			break;
-		case 8:
-			if (board[2][2] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[2][2] = 'x';
-				Continue = true;
-			}
-			break;
-		default:
-			cout << "Opcion no valida" << endl;
-			break;
-		}
-	} while (Continue == false);
-	//////////////After First Input///////////////
-	cout << "This are your controls: " << endl;
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLUMNS; j++)
+
+		} while (Continue == false);
+		//////////////After First Input///////////////
+		cout << "This are your controls: " << endl;
+		for (int i = 0; i < ROWS; i++)
 		{
-			cout << boardoptions[i][j];
+			for (int j = 0; j < COLUMNS; j++)
+			{
+				cout << boardoptions[i][j];
+			}
+			cout << endl;
 		}
-		cout << endl;
+
+		cout << "This is your current board: " << endl;
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLUMNS; j++)
+			{
+				cout << board[i][j];
+			}
+			cout << endl;
+		}
+		if (TurnCount > 4)
+		{
+			CheckWinner = VictoryCheck(board, Player1);
+			if (CheckWinner == true)
+			{
+				WhoWon(TurnCount);
+				return 0;
+			}
+			else if (TurnCount >= 9)
+			{
+				cout << ">>>>>>>>>>>>Its a DRAW<<<<<<<<" << endl;
+				return 0;
+			}
+		}
+
+		TurnCount++;
+
+		////////////////////Waiting for Player2 Input////////////////////
+		do
+		{
+			cout << ">>>>>>>>>>>>>>>>>>>>You are on turn [ " << TurnCount << " ]<<<<<<<<<<<<<<<<<<<<" << endl;
+			cout << "Now, Player 2, select a space where to place your 'O' " << endl;
+			Continue = false;
+			cin >> Player2;
+
+
+			switch (Player2)
+			{
+			case 0:
+				if (board[0][0] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[0][0] = 'O';
+					Continue = true;
+				}
+				break;
+			case 1:
+				if (board[0][1] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[0][1] = 'O';
+					Continue = true;
+				}
+				break;
+			case 2:
+				if (board[0][2] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[0][2] = 'O';
+					Continue = true;
+				}
+				break;
+			case 3:
+				if (board[1][0] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[1][0] = 'O';
+					Continue = true;
+				}
+				break;
+			case 4:
+				if (board[1][1] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[1][1] = 'O';
+					Continue = true;
+				}
+				break;
+			case 5:
+				if (board[1][2] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[1][2] = 'O';
+					Continue = true;
+				}
+				break;
+			case 6:
+				if (board[2][0] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[2][0] = 'O';
+					Continue = true;
+				}
+				break;
+			case 7:
+				if (board[2][1] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[2][1] = 'O';
+					Continue = true;
+				}
+				break;
+			case 8:
+				if (board[2][2] != ' ')
+				{
+					cout << "That space has already been selected" << endl;
+				}
+				else
+				{
+					board[2][2] = 'O';
+					Continue = true;
+				}
+				break;
+			default:
+				cout << "Invalid option" << endl;
+				break;
+			}
+
+		} while (Continue == false);
+
+		////////Rinse and repeat//////
+
+		cout << "This are your controls: " << endl;
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLUMNS; j++)
+			{
+				cout << boardoptions[i][j];
+			}
+			cout << endl;
+		}
+
+		cout << "This is your current board: " << endl;
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLUMNS; j++)
+			{
+				cout << board[i][j];
+			}
+			cout << endl;
+		}
+		if (TurnCount > 4)
+		{
+			CheckWinner = VictoryCheck(board, Player2);
+			if (CheckWinner == true)
+			{
+				WhoWon(TurnCount);
+				return 0;
+			}
+		}
+
+		TurnCount++;
+
+	} while (TurnCount < 10);
+}
+
+
+///////////////////////////////////////Checking for the victory condition//////////////////////////
+bool VictoryCheck(char BoardCheck[3][3], int LastPInput)
+{
+	int X = LastPInput / 3;
+	int Y = LastPInput % 3;
+	bool Win = false;
+
+	if (BoardCheck[X][0] == BoardCheck[X][1] && BoardCheck[X][0] == BoardCheck[X][2])
+	{
+		Win = true;
+		cout << "Case 1" << endl;
 	}
-
-	cout << "This is your current board: " << endl;
-	for (int i = 0; i < ROWS; i++)
+	if (BoardCheck[0][Y] == BoardCheck[1][Y] && BoardCheck[0][Y] == BoardCheck[2][Y])
 	{
-		for (int j = 0; j < COLUMNS; j++)
-		{
-			cout << board[i][j];
-		}
-		cout << endl;
+		Win = true;
+		cout << "Case 2" << endl;
 	}
-	////////////////////Waiting for Player2 Input////////////////////
-	do
+	if (LastPInput == 0 || LastPInput == 4 || LastPInput == 8)
 	{
-	cout << "Now, Player 2, select a space where to place your 'O' " << endl;
-	Continue = false;
-	cin >> Player2;
-	
-
-		switch (Player2)
+		if (BoardCheck[0][0] == BoardCheck[1][1] && BoardCheck[0][0] == BoardCheck[2][2])
 		{
-		case 0:
-			if (board[0][0] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[0][0] = 'O';
-				Continue = true;
-			}
-			break;
-		case 1:
-			if (board[0][1] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[0][1] = 'O';
-				Continue = true;
-			}
-			break;
-		case 2:
-			if (board[0][2] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[0][2] = 'O';
-				Continue = true;
-			}
-			break;
-		case 3:
-			if (board[1][0] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[1][0] = 'O';
-				Continue = true;
-			}
-			break;
-		case 4:
-			if (board[1][1] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[1][1] = 'O';
-				Continue = true;
-			}
-			break;
-		case 5:
-			if (board[1][2] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[1][2] = 'O';
-				Continue = true;
-			}
-			break;
-		case 6:
-			if (board[2][0] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[2][0] = 'O';
-				Continue = true;
-			}
-			break;
-		case 7:
-			if (board[2][1] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[2][1] = 'O';
-				Continue = true;
-			}
-			break;
-		case 8:
-			if (board[2][2] != ' ')
-			{
-				cout << "Ese espacio ya se selecciono, elige otro" << endl;
-			}
-			else
-			{
-				board[2][2] = 'O';
-				Continue = true;
-			}
-			break;
-		default:
-			cout << "Opcion no valida" << endl;
-			break;
+			Win = true;
+			cout << "Case 3" << endl;
 		}
-	} while (Continue == false);
-
-	////////Rinse and repeat//////
-
-	cout << "This are your controls: " << endl;
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLUMNS; j++)
-		{
-			cout << boardoptions[i][j];
-		}
-		cout << endl;
 	}
-
-	cout << "This is your current board: " << endl;
-	for (int i = 0; i < ROWS; i++)
+	if (LastPInput == 2 || LastPInput == 4 || LastPInput == 6)
 	{
-		for (int j = 0; j < COLUMNS; j++)
+		if (BoardCheck[2][0] == BoardCheck[1][1] && BoardCheck[2][0] == BoardCheck[0][2])
 		{
-			cout << board[i][j];
+			Win = true;
+			cout << "Case 4" << endl;
 		}
-		cout << endl;
 	}
+	return Win;
+}
+////////////////Checking Who won based on the Tunr Number///////////////////////////////
+void WhoWon(int TurnCheck)
+{
+	if (TurnCheck % 2 == 0)
+	{
+		cout << "O Wins This one!!!!" << endl;
 
-
-	} while(Turns == false);
+	}
+	else
+	{
+		cout << "X Wins This one!!!!" << endl;
+	}
 }
